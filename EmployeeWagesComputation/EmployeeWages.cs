969b59170f89e1df000294373632fe65f2e9d94e
Takkoli Nithin Kumar Reddy <nithinkumarreddy.t@tvsnext.com>
2023-07-11 11:27:02 +0530

    [Nithin] Add: New feature EmpWages

--- EmployeeWagesComputation/EmployeeWages.cs
+++ EmployeeWagesComputation/EmployeeWages.cs
@@ -8,5 +8,21 @@ namespace EmployeeWagesComputation
 {
     internal class EmployeeWages
     {
+        const int wage_per_hr = 20, full_day_hr = 8;
+        public  void EmployeeAttendence()
+        {
+            
+            Random random = new Random();
+            int expCheck = random.Next(0, 2);
+            if (expCheck == 0)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Absent");
+        }
+        public void CalculateExpWage()
+        {
+            int dailyExpWage = wage_per_hr * full_day_hr;
+            Console.WriteLine(dailyExpWage);
+        }
     }
 }
